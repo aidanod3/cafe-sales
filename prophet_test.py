63ab241fad67278575cc8f25b16975150513d3aa
aidan <aidanod3@gmail.com>
2025-10-16 11:17:10 -0400

    testing prophet

---
+++ prophet_test.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from prophet import Prophet
+import matplotlib.pyplot as plt
+
+# tsv path
+file_path = "mapreduce_output/daily_revenue_per_store.tsv"
+
+# read tsv into dataframe
+df = pd.read_csv(file_path, sep='\t', names=['date_store', 'daily_revenue'])
+
+# split composite key into 'date' and 'store_id'
+df[['date', 'store_id']] = df['date_store'].str.split('|', expand=True)
+
+# convert data types from string
+df['daily_revenue'] = df['daily_revenue'].astype(float)
+df['date'] = pd.to_datetime(df['date'], format='%m/%d/%y')
+df = df.sort_values(['date', 'store_id']).reset_index(drop=True)
+
+# remove date_store col
+df = df[['date', 'store_id', 'daily_revenue']]
+
+# rename cols
+df = df.rename(columns={'date': 'ds', 'daily_revenue': 'y'})
+
+# separate into training and testing data
+last_index = df.index[-1]
+training_data_max = round(last_index * 0.8) # 119292
+
+training_data = df[df.index <= training_data_max] # (0 -> 434)
+testing_data = df[df.index > training_data_max] # (435 -> 542)
+
+# group training data by store_id
+grouped = training_data.groupby('store_id')
+
+# degroup into separate df's and remove store_id col
+df_store3 = grouped.get_group('3')[['ds', 'y']]
+df_store5 = grouped.get_group('5')[['ds', 'y']]
+df_store8 = grouped.get_group('8')[['ds', 'y']]
+
+# print(df_store3)
+# print(df_store5)
+# print(df_store8)
+
+m = Prophet()
+m.fit(df_store3)
+
+future = m.make_future_dataframe(periods=36)
+print(future)
+
+forecast = m.predict(future)
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(36))
+
+fig1 = m.plot(forecast)
+plt.show()
---
+++ run_mapreduce.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+
+# hadoop directories
+hdfs_input_dir = "/coffee-sales-project/coffee_sales_input"
+hdfs_output_dir = "/coffee-sales-project/coffee_sales_output"
+
+# local paths
+local_input = "coffee_shop_sales.csv"
+local_output_dir = "mapreduce_output"
+
+# store in mapreduce_output/daily_revenue_per_store.tsv
+local_output_file = os.path.join(local_output_dir, "daily_revenue_per_store.tsv")
+
+# check if local output directory exists
+os.makedirs(local_output_dir, exist_ok=True)
+
+# upload input file to hdfs
+print("Uploading input file to HDFS...")
+subprocess.run(["hdfs", "dfs", "-mkdir", "-p", hdfs_input_dir], check=True)
+subprocess.run(["hdfs", "dfs", "-put", "-f", local_input, hdfs_input_dir], check=True)
+
+# remove previous hdfs output if exists
+print("Removing old HDFS output if it exists...")
+subprocess.run(["hdfs", "dfs", "-rm", "-r", "-f", hdfs_output_dir], check=True)
+
+# hadoop streaming jar location
+hadoop_streaming_jar = "/opt/homebrew/Cellar/hadoop/3.4.2/libexec/share/hadoop/tools/lib/hadoop-streaming-3.4.2.jar"
+
+# run hadoop streaming job
+subprocess.run([
+    "hadoop", "jar", hadoop_streaming_jar,
+    "-input", os.path.join(hdfs_input_dir, "coffee_shop_sales.csv"),
+    "-output", hdfs_output_dir,
+    "-mapper", "mapper.py",
+    "-reducer", "reducer.py",
+    "-file", "mapper.py",
+    "-file", "reducer.py"
+], check=True)
+
+# check hdfs if output files exist
+print("Verifying HDFS output files...")
+result = subprocess.run(["hdfs", "dfs", "-ls", hdfs_output_dir], capture_output=True, text=True, check=True)
+print(result.stdout)
+
+# copy hdfs output into local output directory
+subprocess.run([
+    "hdfs", "dfs", "-getmerge",
+    f"{hdfs_output_dir}/*",
+    local_output_file
+], check=True)
+
+print(f"MapReduce job complete. Output saved to: {local_output_file}")
+
+# note: the output isn't being sorted by date, due to the un-padded date format.
